341457c8d4c96a21b450a67e0b4da1878397f328
Karthik Appiah <92764106+karthikappiah@users.noreply.github.com>
2021-11-17 05:40:10 -0800

    back to square one
    CRUD for users without the U

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,6 @@ const { PrismaClient } = require("@prisma/client");
 
 const { users } = new PrismaClient();
 
-// create
 router.post("/", async (req, res) => {
 	const { username, password } = req.body;
 	const result = await users.create({
@@ -15,35 +14,18 @@ router.post("/", async (req, res) => {
 	res.json(result);
 });
 
-// reads all users
 router.get("/", async (req, res) => {
-	const all_users = await users.findMany({
-		select: {
-			user_id: true,
-			username: true,
-			password: true,
-		},
-	});
-	res.json(all_users);
+	const result = await users.findMany();
+	res.json(result);
 });
 
-// reads user based on id
-router.get("/:id", async (req, res) => {
-	const { user_id } = req.params;
-	const user = await users.findFirst({
-		where: {
-			user_id: Number(user_id),
-		},
-	});
-	res.json(user);
-});
-
-// update
 router.put("/:id", async (req, res) => {
-	const user_id = Number(req.params.id);
+	const user_id = req.params;
 	const { username, password } = req.body;
 	const result = await users.update({
-		where: { user_id },
+		where: {
+			user_id,
+		},
 		data: {
 			username,
 			password,
@@ -51,15 +33,14 @@ router.put("/:id", async (req, res) => {
 	});
 	res.json(result);
 });
-
-// delete
 router.delete("/:id", async (req, res) => {
-	const user_id = req.params.id;
+	const id = Number(req.params.id);
 	const result = await users.delete({
 		where: {
-			user_id: Number(user_id),
+			id,
 		},
 	});
 	res.json(result);
 });
+
 module.exports = router;
